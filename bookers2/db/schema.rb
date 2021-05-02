8adb7a87d6b49d53dfef9d5985704a48c401c065
tmy628 <tmy6h1r@gmail.com>
2021-05-02 08:49:12 +0000

    Relationshipモデルを作成

--- bookers2/db/schema.rb
+++ bookers2/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_02_055139) do
+ActiveRecord::Schema.define(version: 2021_05_02_083002) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 2021_05_02_055139) do
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
