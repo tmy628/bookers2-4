0eed28930de11b63a33fc293245ad6da52564d91
tmy628 <tmy6h1r@gmail.com>
2021-05-02 12:10:02 +0000

    アソシエーションを記述

--- bookers2/app/models/user.rb
+++ bookers2/app/models/user.rb
@@ -6,9 +6,16 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  # UserモデルとBookCommentモデルを関連付ける
   has_many :favorites, dependent: :destroy
-  # Userモデルに関連付けを追加する
+
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # foreign_key: "followed_id"でどのカラムを参照して欲しいのかを定義する
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  # 被フォロー関係を通じてfollowed_idをフォローしている人を参照する
+
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id" ,dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+  # # 与フォロー関係を通じてfollower_idをフォローしている人を参照
 
   attachment :profile_image
 
